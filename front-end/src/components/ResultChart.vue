41e06476a3ac30600b9b23d2aa3ac1743a5674e5
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-01-19 22:21:43 +0700

    feat: add pacckage chartjs, add component ResultChart, update page

---
+++ front-end/src/components/ResultChart.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <canvas
+      id="myChart"
+      style="width:auto; max-width:700px; max-height: 400px;"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue';
+import { Chart, registerables } from 'chart.js';
+
+const xValues = ['Sopan', 'Nasir kuning'];
+const yValues = [102, 140];
+const barColors = [
+  '#2b5797',
+  '#e8c3b9',
+];
+
+Chart.register(...registerables);
+
+export default defineComponent({
+  name: 'ResultChart',
+
+  setup() {
+    onMounted(() => {
+      // eslint-disable-next-line no-new
+      new Chart('myChart', {
+        type: 'pie',
+        data: {
+          labels: xValues,
+          datasets: [{
+            backgroundColor: barColors,
+            data: yValues,
+          }],
+        },
+        options: {},
+      });
+    });
+  },
+});
+
+</script>
--- front-end/src/pages/VotingEventResult.vue
+++ front-end/src/pages/VotingEventResult.vue
@@ -1,12 +1,34 @@
 <template>
   <q-page padding>
-    VotingEventResult: {{ voting }}
+    <!-- VotingEventResult: {{ voting }} -->
+    <q-card class="my-card">
+      <q-card-section>
+        <div class="col text-center">
+          {{ up }}
+          <q-separator color="dark" />
+          {{ down }}
+        </div>
+      </q-card-section>
+    </q-card>
+    <div class="q-mt-xl">
+      <result-chart />
+    </div>
   </q-page>
 </template>
 
 <script lang="ts" setup>
 import { inject, Ref } from 'vue';
 import { VotingEvent } from '@evote/core';
+import ResultChart from 'src/components/ResultChart.vue';
 
 const voting = inject<Ref<VotingEvent>>('VotingEvent')!;
+const up = 900;
+const down = 1000;
+
 </script>
+
+<style lang="sass">
+  .my-card
+    width: 100%
+    max-width: 80px
+</style>
